f90afab57637ca42f8c8b253b335aeafba475f30
villhelm-e <villhelm.e@gmail.com>
2021-06-21 13:10:32 -0700

    Added button, OnClick event, and refactored some grid generation into functions.

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,35 @@ var gridSize = 16;
 
 var squareSize = 960/gridSize;
 
-//add squares to board
-for (x=0; x<(gridSize**2); x++) {
-    const square = document.createElement('div');
-    square.classList.add('pixel');
-    square.style.width = squareSize + "px";
-    square.style.height = squareSize + "px";
-    board.appendChild(square);
+function setPixels() {
+    
+    for (x=0; x<(gridSize**2); x++) {
+        const square = document.createElement('div');
+        square.classList.add('pixel');
+        square.style.width = squareSize + "px";
+        square.style.height = squareSize + "px";
+        board.appendChild(square);
+    }
 }
 
-//Add mouse enter listener to all squares
-const pixels = document.querySelectorAll(".pixel").forEach(item => {
-    item.addEventListener('mouseenter', event => {
-        item.classList.add('etched');
+function listen() {
+    const pixels = document.querySelectorAll(".pixel");
+
+    //Add mouse enter listener to all squares
+    pixels.forEach(item => {
+        item.addEventListener('mouseenter', event => {
+            item.classList.add('etched');
+        });
     });
-});
+}
+
+function clearBoard() {
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach(item => {
+        item.classList.remove('etched');
+    })
+}
+
+setPixels();
+listen();
