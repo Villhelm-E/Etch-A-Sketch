bca8de465be51b3deed37d52c350337c509739ca
villhelm-e <villhelm.e@gmail.com>
2021-06-21 08:41:57 -0700

    Set up flexbox and dynamically calculating the size of each square in the grid.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@
 const container = document.querySelector('#board');
 var gridSize = 16;
 
+var squareSize = 960/gridSize;
+
 //add 9 squares
 for (x=0; x<(gridSize**2); x++) {
     const square = document.createElement('div');
     square.classList.add('content');
     square.textContent = "test";
+    square.style.width = squareSize + "px";
+    square.style.height = squareSize + "px";
     board.appendChild(square);
 }
