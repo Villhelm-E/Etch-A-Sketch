1caf98751a4d223e234d656359488242ea10f661
villhelm-e <villhelm.e@gmail.com>
2021-06-21 11:30:36 -0700

    Changed content class to pixel class

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,15 @@ var squareSize = 960/gridSize;
 //add squares to board
 for (x=0; x<(gridSize**2); x++) {
     const square = document.createElement('div');
-    square.classList.add('content');
+    square.classList.add('pixel');
     square.style.width = squareSize + "px";
     square.style.height = squareSize + "px";
     board.appendChild(square);
 }
 
 //Add mouse enter listener to all squares
-const pixels = document.querySelectorAll(".content").forEach(item => {
+const pixels = document.querySelectorAll(".pixel").forEach(item => {
     item.addEventListener('mouseenter', event => {
-        item.style.backgroundColor = "black";
+        item.style.backgroundColor = "cde";
     });
 });
