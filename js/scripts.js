39424665640528dd01eee9593f2fdc70bffee1be
villhelm-e <villhelm.e@gmail.com>
2021-06-21 13:19:43 -0700

    Got the remove pixels and readding them to work.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 const board = document.querySelector('#board');
 var gridSize = 16;
 
-var squareSize = 960/gridSize;
-
 function setPixels() {
     
+    var squareSize = 960/gridSize;
+
     for (x=0; x<(gridSize**2); x++) {
         const square = document.createElement('div');
         square.classList.add('pixel');
@@ -13,6 +13,8 @@ function setPixels() {
         square.style.height = squareSize + "px";
         board.appendChild(square);
     }
+
+    listen();
 }
 
 function listen() {
@@ -32,6 +34,19 @@ function clearBoard() {
     pixels.forEach(item => {
         item.classList.remove('etched');
     })
+}
+
+function deletePixels() {
+    while (board.firstChild) {
+        board.removeChild(board.lastChild);
+    }
+}
+
+function resetBoard() {
+    deletePixels();
+
+    gridSize = prompt("Choose a resolution.", 16);
+    setPixels();
 }
 
 setPixels();
