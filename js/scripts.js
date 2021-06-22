0d0ad14a784e53f08774176cacee65c5ed6e2f20
villhelm-e <villhelm.e@gmail.com>
2021-06-22 13:51:49 -0700

    Added 'Darken' draw mode

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //variables
 const board = document.querySelector('#board');
 var gridSize = 16;
+var mode = "normal";
 
 //calculates pixel size and adds them to the board
 function setPixels() {
@@ -26,7 +27,49 @@ function listen() {
     //Add mouse enter listener to all squares
     pixels.forEach(item => {
         item.addEventListener('mouseenter', event => {
-            item.classList.add('etched');
+            //normal draw mode
+            if (mode == "normal") {
+                item.classList.add('etched');
+            }
+            //darken draw mode
+            else if (mode == "darken") {
+                if (!item.classList.contains("ten")) {
+                    item.classList.add("ten");
+                }
+                else if (!item.classList.contains("twenty")) {
+                    item.classList.add("twenty");
+                }
+                else if (!item.classList.contains("thirty")) {
+                    item.classList.add("thirty");
+                }
+                else if (!item.classList.contains("forty")) {
+                    item.classList.add("forty");
+                }
+                else if (!item.classList.contains("fifty")) {
+                    item.classList.add("fifty");
+                }
+                else if (!item.classList.contains("sixty")) {
+                    item.classList.add("sixty");
+                }
+                else if (!item.classList.contains("seventy")) {
+                    item.classList.add("seventy");
+                }
+                else if (!item.classList.contains("eighty")) {
+                    item.classList.add("eighty");
+                }
+                else if (!item.classList.contains("ninety")) {
+                    item.classList.add("ninety");
+                }
+                else if (!item.classList.contains("hundred")) {
+                    item.classList.add("hundred");
+                }
+                else {
+                    //nothing
+                }
+            }
+            else {
+                item.classList.add('etched');
+            }
         });
     });
 }
@@ -36,7 +79,23 @@ function clearBoard() {
     const pixels = document.querySelectorAll(".pixel");
 
     pixels.forEach(item => {
-        item.classList.remove('etched');
+        //normal draw mode
+        if (mode == "normal") {
+            item.classList.remove('etched');
+        }
+        //darken draw mode
+        else if (mode == "darken") {
+            item.classList.remove("ten");
+            item.classList.remove("twenty");
+            item.classList.remove("thirty");
+            item.classList.remove("forty");
+            item.classList.remove("fifty");
+            item.classList.remove("sixty");
+            item.classList.remove("seventy");
+            item.classList.remove("eighty");
+            item.classList.remove("ninety");
+            item.classList.remove("hundred");
+        }
     })
 }
 
@@ -69,6 +128,40 @@ function resetBoard() {
         //add pixels to board
         setPixels();
     }
+}
+
+//set up radio buttons
+const radio = document.querySelectorAll('input[name=mode]');
+
+//add event listeners to radio buttons
+radio.forEach(item => {
+    item.addEventListener('change', event => {
+        //manage draw modes
+        switch (item.id) {
+            case "normal":  
+                normal();
+                break;
+            case "darken":
+                darken();
+                break;
+            default:
+                normal();
+        }
+    });
+});
+
+//sets board to normal draw mode
+function normal() {
+    mode = "normal";
+    deletePixels();
+    setPixels()
+}
+
+//sets board to darken draw mode
+function darken() {
+    mode = "darken";
+    deletePixels();
+    setPixels();
 }
 
 //Initialize the board for first time
